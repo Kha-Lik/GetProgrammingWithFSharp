6c16767ae1e5607670045c25324787afb4e0a8d6
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-10-12 22:03:43 +0300

    Add code and data for 30th lesson

---
+++ Scripts/lesson-30.fs
@@ -0,0 +1,18 @@
+﻿module Lesson30.lesson_30
+
+open FSharp.Data
+open XPlot.GoogleCharts
+
+[<Literal>]
+let ResolutionFolder = __SOURCE_DIRECTORY__
+
+type Football = CsvProvider<"FootballResults.csv", ResolutionFolder=ResolutionFolder>
+let data = Football.GetSample().Rows |> Seq.toArray
+
+data
+|> Seq.filter (fun row -> row.``Full Time Home Goals`` > row.``Full Time Away Goals``)
+|> Seq.countBy (fun row -> row.``Home Team``)
+|> Seq.sortByDescending snd
+|> Seq.take 10
+|> Chart.Column
+|> Chart.Show
---
+++ Scripts/lesson-30.fsx
@@ -0,0 +1,19 @@
+﻿#r "nuget: FSharp.Data"
+#r "nuget: XPlot.GoogleCharts"
+
+open FSharp.Data
+open XPlot.GoogleCharts
+
+[<Literal>]
+let ResolutionFolder = __SOURCE_DIRECTORY__
+
+type Football = CsvProvider<"FootballResults.csv", ResolutionFolder=ResolutionFolder>
+let data = Football.GetSample().Rows |> Seq.toArray
+
+data
+|> Seq.filter (fun row -> row.``Full Time Home Goals`` > row.``Full Time Away Goals``)
+|> Seq.countBy (fun row -> row.``Home Team``)
+|> Seq.sortByDescending snd
+|> Seq.take 10
+|> Chart.Column
+|> Chart.Show
